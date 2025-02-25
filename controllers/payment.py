13826be1a3445d50e027bfcc2589817013b3afad
unknown <yesthanhnhan16@gmail.com>
2025-02-25 15:04:50 +0700

    update vnpay ipn url

--- controllers/payment.py
+++ controllers/payment.py
@@ -82,12 +82,13 @@ class VNPayController(http.Controller):
     def process_payment(self, **post):
         try:
             secret_key = "vnpay@MERCHANT"  # Secret key cần được bảo mật
+            _logger.info(post)
             _logger.info(post.get("code", "Lỗi"))
             _logger.info(request.jsonrequest)
 
             # Kiểm tra checksum
             if not self.verify_checksum(post, secret_key):
-                return request.make_json_response({"code": "06", "message": "Sai thông tin xác thực"})
+                return {"code": "06", "message": "Sai thông tin xác thực"}
             _logger.info(post)
             # Lưu giao dịch vào cơ sở dữ liệu Odoo (ví dụ lưu vào một model `payment.transaction`)
             # request.env['payment.transaction'].sudo().create({
@@ -101,14 +102,13 @@ class VNPayController(http.Controller):
 
             # Trả về phản hồi thành công
            
-            return  request.make_json_response({
+            return  {
                 "code": "03",
                 "message": "Đơn hàng đã được thanh toán",
                 "data": {"txnId": post.get("txnId")}
-            })
+            }
         except Exception as e:
-             
-            return request.make_json_response({"code": "06", "message": "Lỗi hệ thống", "error": str(e)})
+            return{"code": "06", "message": "Lỗi hệ thống", "error": str(e)}
 
     @http.route(
         "/payment/vnpay/create_invoice",
