c03789d9df690c720c606263b1f8ed1080f04287
unknown <yesthanhnhan16@gmail.com>
2025-02-27 18:50:32 +0700

    add process refund

--- models/payment_provider.py
+++ models/payment_provider.py
@@ -270,9 +270,9 @@ class PaymentProviderVNPay(models.Model):
         provider = self.sudo().search([('code', '=', 'vnpay')], limit=1)
         
         if int(amount)<0:
-            pos_order = self.env["pos.order"].search([["id","=", pos_oder_id]], limit=1)
+            pos_order = self.env["pos.order"].search([("id","=", pos_oder_id)], limit=1)
             _logger.info(pos_order.refunded_order_id.id)
-            payment_transaction=self.env["payment.transaction"].search([["pos_order_id", "=", pos_order.refunded_order_id.id]])
+            payment_transaction=self.env["payment.transaction"].search([("pos_order_id", "=", pos_order.refunded_order_id.id)], limit=1)
             _logger.info(payment_transaction.provider_id.code)
             if payment_transaction.provider_id.code != "vnpay":
                 return {
