2a7146d0d48255911f4bd2251a32110423616ab1
unknown <yesthanhnhan16@gmail.com>
2025-03-06 08:26:31 +0700

    fix error in payment screen

--- static/src/online_payment_popup/numpad.js
+++ static/src/online_payment_popup/numpad.js
@@ -1,12 +1,12 @@
 import { Numpad } from "@point_of_sale/app/generic_components/numpad/numpad";
 import { patch } from "@web/core/utils/patch";
 patch(Numpad.prototype, {
-  setup() {
-    if (!this.props.onClick) {
-      this.numberBuffer = useService("custom_number_buffer");
-      this.onClick = (buttonValue) => this.numberBuffer.sendKey(buttonValue);
-    } else {
-      this.onClick = this.props.onClick;
-    }
-  },
+  // setup() {
+  //   if (!this.props.onClick) {
+  //     this.numberBuffer = useService("custom_number_buffer");
+  //     this.onClick = (buttonValue) => this.numberBuffer.sendKey(buttonValue);
+  //   } else {
+  //     this.onClick = this.props.onClick;
+  //   }
+  // },
 });
