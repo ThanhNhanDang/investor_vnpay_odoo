057e3449c56282ceb5080e508171f8f17f82fa13
unknown <yesthanhnhan16@gmail.com>
2025-03-18 13:13:10 +0700

    updadte: create product => create stock.quant

--- models/pos_payment_method.py
+++ models/pos_payment_method.py
@@ -8,9 +8,15 @@ from odoo import _, api, fields, models
 
 _logger = logging.getLogger(__name__)
 
-
 class POSVNPayPaymentMethod(models.Model):
     _inherit = "pos.payment.method"
+    is_online_payment = fields.Boolean(
+        string="Online Payment", help="Use this payment method for online payments (payments made on a web page with online payment providers)", default=False)
+    online_payment_provider_ids = fields.Many2many(
+        'payment.provider', string="Allowed Providers", domain="[('is_published', '=', True), ('state', 'in', ['enabled', 'test'])]")
+    has_an_online_payment_provider = fields.Boolean(
+        compute='_compute_has_an_online_payment_provider', readonly=True)
+    type = fields.Selection(selection_add=[('online', 'Online')])
 
     code = fields.Char(
         string="Code", help="The technical code of this payment method.", readonly=True
--- models/product_template_inherit.py
+++ models/product_template_inherit.py
@@ -2,7 +2,22 @@ from odoo import api, fields, models, http, exceptions
 import logging
 _logger = logging.getLogger(__name__)
 
+
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     color_background = fields.Char(string="Màu nền gói tiền")
     is_device = fields.Boolean(string="Thiết bị POS")
+
+    def create(self, vals_list):
+        res = super(ProductTemplate, self).create(vals_list)
+        if res.is_storable:
+            default_product_id = self.env.context.get('default_product_id', len(
+                res.product_variant_ids) == 1 and res.product_variant_id.id)
+            change_product_qty = self.env['stock.change.product.qty'].create({
+                'product_id': default_product_id,
+                'product_tmpl_id': res.id,
+                'new_quantity': 1
+            })
+            change_product_qty.change_product_qty()
+        return res
+        
