9bb8bbc2f4ad6b22db68db723ab669f49c9a74be
ThanhNhanDev <yesthanhnhan@gmail.com>
2025-03-19 16:10:39 +0700

    update rule

--- static/src/online_payment_popup/payment_screen.js
+++ static/src/online_payment_popup/payment_screen.js
@@ -155,21 +155,26 @@ patch(PaymentScreen.prototype, {
   },
 
   async printQRCode(onlinePaymentData) {
-    const link = document.createElement("a");
-    const currentDate = formatDateTime(luxon.DateTime.now(), {
-      format: "MM_dd_yyyy-HH_mm_ss",
-    });
-    const companyName =
-      this.env.services.company.currentCompany.name.replaceAll(" ", "_");
-    link.download = `${companyName}-${currentDate}.png`;
-    const data = this.pos.orderExportForPrinting(order);
-    const Jpeg = await this.renderer.toJpeg(
-      OnlinePaymentPopup,
-      onlinePaymentData,
-      {}
+    const order = this.pos.models["pos.order"].getBy(
+      "uuid",
+      this.currentOrder.uuid
     );
-    link.href = png.toDataURL().replace("data:image/jpeg;base64,", "");
-    link.click();
+    order.tracking_number = "S" + order.tracking_number;
+    // const link = document.createElement("a");
+    // const currentDate = formatDateTime(luxon.DateTime.now(), {
+    //   format: "MM_dd_yyyy-HH_mm_ss",
+    // });
+    // const companyName =
+    //   this.env.services.company.currentCompany.name.replaceAll(" ", "_");
+    // link.download = `${companyName}-${currentDate}.png`;
+    // const data = this.pos.orderExportForPrinting(order);
+    // const Jpeg = await this.renderer.toJpeg(
+    //   OnlinePaymentPopup,
+    //   onlinePaymentData,
+    //   {}
+    // );
+    // link.href = png.toDataURL().replace("data:image/jpeg;base64,", "");
+    // link.click();
   },
 
   //@override
