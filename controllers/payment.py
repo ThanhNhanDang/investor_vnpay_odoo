8efe89b2e107beabd86b11a0ffd9655a7012c616
ThanhNhanDev <yesthanhnhan@gmail.com>
2025-03-19 16:42:24 +0700

    update rule

--- controllers/payment.py
+++ controllers/payment.py
@@ -116,6 +116,7 @@ class VNPayController(http.Controller):
           
             # Gửi thông báo qua Bus
             ids = data.get("txnId").split("a")
+            _logger.info(ids)
             transaction = self._create_transaction(data.get("amount"), ids[0], data.get("txnId"), ids[1], ids[2], data.get("qrTrace"))
             transaction._process_pos_online_payment()
             request.env['bus.bus']._sendone(
