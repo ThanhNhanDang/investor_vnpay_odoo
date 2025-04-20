c8db4afc7066e053fd954eafe94e10778a57355c
unknown <yesthanhnhan16@gmail.com>
2025-04-20 18:25:54 +0700

    update

--- static/src/online_payment_popup/web_socket_service.js
+++ static/src/online_payment_popup/web_socket_service.js
@@ -26,7 +26,7 @@ export const webSocketService = {
 
     function connect() {
       if (isDisconnect) return;
-      const wsUri = "ws://127.0.0.1:5000/";
+      const wsUri = "ws://localhost:5000";
       try {
         websocket = new WebSocket(wsUri);
       } catch (error) {
